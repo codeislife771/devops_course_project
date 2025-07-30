98935131c515afc45d18b9b501034b63a2290e54
devops <hengorg@2019@gmail.com>
2025-07-30 11:33:04 +0300

    feat: Read DEBUG value from environment variable for dynamic Flask config

--- flask-task-manager/app.py
+++ flask-task-manager/app.py
@@ -74,4 +74,5 @@ def delete_task(name):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True, port=port)
+    debug = os.environ.get('DEBUG', 'false').lower() == 'true'
+    app.run(host='0.0.0.0', debug=debug, port=port)
